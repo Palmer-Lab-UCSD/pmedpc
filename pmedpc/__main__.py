96cae6be6412ba47416a95d575d5492de807a615
Robert Vogel <12845765+robert-vogel@users.noreply.github.com>
2023-11-09 16:07:45 -0800

    Create __main__.py

---
+++ pmedpc/__main__.py
@@ -0,0 +1,28 @@
+"""
+pmedpc: A tool to extract data from MedPC files and write to csv.
+
+By: Beverly Peng
+"""
+
+_epilog = """
+
+Example
+
+Open specified MedPC file, extract relevant data and write
+to file <filename>_format.csv
+
+python -m pmedpc <filename>
+"""
+
+import sys
+
+from . import utils
+
+def main():
+  args = utils._parse_args(sys.arv[1:])
+
+  raise NotImplementedError
+  return 0
+
+
+main()
